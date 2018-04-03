581accb849c9e02098a8595119c89919858562bb
mbs0221 <mbs2021@163.com>
2018-04-03 19:36:26 +0800

    修正了参数网格生成

--- Regression/runCreateParams.m
+++ Regression/runCreateParams.m
@@ -4,24 +4,27 @@ addpath(genpath('./utils'));
 RangeP1 = 2.^(1:2:10)';
 kernel = struct('kernel', 'rbf', 'p1', RangeP1);
 % 分类器网格搜索参数
-C = 2.^(1:2:10)';
-C1 = 2.^(1:2:10)';
-C2 = 2.^(1:2:10)';
-C3 = 2.^(1:2:6)';
-C4 = 2.^(1:2:6)';
-EPS1 = 2.^(1:2:6)';
-EPS2 = 2.^(1:2:6)';
+C = 2.^(0:2:10)';
+C1 = 2.^(0:2:10)';
+C2 = 2.^(0:2:10)';
+C3 = 2.^(0:2:6)';
+C4 = 2.^(0:2:6)';
+EPS1 = 2.^(0:2:6)';
+EPS2 = 2.^(0:2:6)';
+RHO = 2.^(-2:2:8)';
+LAMBDA = 2.^(-2:2:8)';
 Params0 = struct('Name', 'TWSVR', 'C1', C1, 'C2', C2, 'C3', C3, 'C4', C4, 'eps1', EPS1, 'eps2', EPS2, 'Kernel', kernel);
 Params1 = struct('Name', 'TWSVR_Xu', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'Kernel', kernel);
 Params2 = struct('Name', 'MTL_TWSVR', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'Kernel', kernel);
 Params3 = struct('Name', 'MTL_TWSVR_Xu', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'Kernel', kernel);
+Params4 = struct('Name', 'MTL_TWSVR_Mei', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'rho', RHO, 'lambda', LAMBDA, 'Kernel', kernel);
 % 转换参数表
-OParams = {Params0,Params1,Params2,Params3};
+OParams = {Params0;Params1;Params2;Params3;Params4};
 nParams = length(OParams);
 for i = 1 : nParams
     % 初始化参数表
     fprintf('Params\n')
-    IParams{i, 1} = Classifier.CreateParams(OParams{i});
+    IParams{i, 1} = CreateParams(OParams{i});
 end
 % 保存参数表
 save('LabIParams.mat', 'IParams');
---
+++ Regression/utils/CreateParams.m
@@ -0,0 +1,62 @@
+function [ Params ] = CreateParams(root)
+%CREATEPARAMS 此处显示有关此类的摘要
+% 构造网格参数表
+%   此处显示详细说明
+    
+    nParams = GetParamsCount(root);
+    Params = [];
+    for index = 1 : nParams
+        Params = cat(2, Params, GetParams(root, index));
+    end
+    
+    function [ Count ] = GetPropertyCount(root)
+        % 得到叶节点数目
+        Count = 0;
+        if isstruct(root)
+            names = fieldnames(root);
+            [m, ~] = size(names);
+            for i = 1 : m
+                SubParamTree = root.(names{i});
+                Count = Count + GetPropertyCount(SubParamTree);
+            end
+        else
+            Count = 1;
+        end
+    end
+    function [ Count ] = GetParamsCount(root)
+        % 获取所有参数的组合数
+        Count = 1;
+        if isstruct(root)
+            names = fieldnames(root);
+            [m, ~] = size(names);
+            for i = 1 : m
+                child = root.(names{i});
+                x = GetParamsCount(child);
+                Count = Count * x;
+            end
+        else
+            [m, ~] = size(root);
+            Count = Count * m;
+        end
+    end
+    function [ Params ] = GetParams(root, index)
+        names = fieldnames(root);
+        [m, ~] = size(names);
+        % 分解下标
+        for i = 1 : m
+            name = names{i};
+            child = root.(name);
+            wi = GetParamsCount(child);
+            % 取得当前位的idx、得到高位num
+            idx = mod(index-1, wi)+1;
+            index = ceil(index / wi);
+            % 访问子节点
+            if isstruct(child)
+                Params.(name) = GetParams(child, idx);
+            else
+                Params.(name) = child(idx, :);
+            end
+        end
+    end 
+    
+end
