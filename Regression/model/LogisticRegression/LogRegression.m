01c05128944327fb2408d0d742f75865c04e415c
mbs0221 <mbs2021@163.com>
2018-04-10 19:49:39 +0800

    添加实验数据集

--- Regression/model/LogisticRegression/LogRegression.m
+++ Regression/model/LogisticRegression/LogRegression.m
@@ -1,42 +1,62 @@
-function [  ] = LogRegression( X, Y, Optimizer, NumIter )
+function [ yTest, Time ] = LogRegression( xTrain, yTrain, xTest, opts)
 %LOGREGRESSION 此处显示有关此函数的摘要
+% Logistic Regression
 %   此处显示详细说明
-    if ismatrix(X) == 1
-        [m, ~] = size(X);
-    end
-    % 左边添加一列
-    e = ones(m, 1);
-    X = [e X];
-    % 输出矩阵大小
-    [m, n] = size(X);
-    fprintf('size = [%d, %d]\n', m, n);
+
+    Optimizer = opts.Optimizer;
+    NumIter = opts.NumIter;
+    
     % Logistic梯度优化
     if Optimizer == 'SGA'
-        W = SGA(X, Y, NumIter);
+        W = SGA(xTrain, yTrain, NumIter);
     else
-        W = BGA(X, Y, NumIter);
+        W = BGA(xTrain, yTrain, NumIter);
     end
-    % 绘图
-    fg = figure(1);
-    title('LR-BGA');
-    xlabel('x1');
-    ylabel('x2');
-    % 绘制所有点
-    %scatter(X(:,2), X(:,3), 1, '*');
-    %hold on;
-    % 绘制正类点
-    XP = X(Y(:,1)==1,:);
-    scatter(XP(:,2), XP(:,3), 12, '+');
-    hold on;
-    % 绘制负类点
-    XN = X(Y(:,1)==0,:);
-    scatter(XN(:,2), XN(:,3), 12, 'O');
-    hold on;
-    % 绘制拟合曲线
-    x = -3.0:0.1:3.0;
-    y = (-W(1)-W(2)*x)/W(3);
-    plot(x, y);
-    % 保存图片
-    saveas(fg, ['../images/', 'LR-BGA.png']);
+    
+    
+
+%% 批量梯度上升
+    function [ W ] = BGA( X, Y, NumIter )
+    %BGD 此处显示有关此函数的摘要
+    %   此处显示详细说明
+        if ismatrix(X) == 1
+            [~, n] = size(X);
+        end
+
+        alpha = 0.001;
+        W = ones(n, 1);
+
+        for k = 1 : NumIter
+            H = Sigmoid(X * W);
+            E = Y - H;
+            W = W + alpha * X.' * E; % 一阶梯度
+            % 二阶牛顿法
+    %         D1 = -X.'*E;
+    %         D2 = X.'.*X.*H.*(1-H);
+    %         W = W + alpha * D2\D1;
+        end
+    end
+
+%% 随机梯度上升
+    function [ W ] = SGA( X, Y, NumIter )
+    %SGA 此处显示有关此函数的摘要
+    %   此处显示详细说明
+        if ismatrix(X) == 1
+            [m, n] = size(X);
+        end
+    %    alpha = 0.01;
+        weights = ones(n);
+        for j = 1 : NumIter
+            for i = 1 : m
+                alpha = 4 / (1.0 + j + i) + 0.01;
+                rand_index = randperm(m, 1);
+                h = Sigmoid(sum(X(rand_index) * weights));
+                e = Y(rand_index) - h;
+                weights = weights + alpha*e*X(rand_index);
+            end
+        end
+        W = weights;
+    end
+
 end
 
