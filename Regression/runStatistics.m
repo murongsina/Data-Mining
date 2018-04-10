a4600baeb5ceb5ec1dc5538d849525fa72edf6b7
mbs0221 <mbs2021@163.com>
2018-04-10 21:40:08 +0800

    添加数据集结果统计

---
+++ Regression/runStatistics.m
@@ -0,0 +1,30 @@
+Path = './data/';
+
+% 添加搜索路径
+addpath(genpath('./model'));
+addpath(genpath('./utils'));
+addpath(genpath('./utils/params/'));
+
+% 加载数据集和网格搜索参数
+load('LabUCIReg.mat');
+load('LabMulti.mat');
+load('LabIParams.mat');
+
+% 实验数据集
+LabDataSets = {LabUCIReg, LabMulti};
+% 统计每个数据集上的多任务实验数据
+index = 0;
+for i = 1 : 2
+    DataSets = LabDataSets{i};
+    n = length(DataSets);
+    for j = 1 : n
+        index = index + 1;
+        DataSet = DataSets(j);
+        [ LabStat, HasStat ] = LabStatistics(Path, DataSet, IParams);
+        if HasStat == 1
+            StatPath = [Path, 'LabStat-', DataSet.Name, '.mat'];
+            fprintf('save: %s\n', StatPath);
+            save(StatPath, 'LabStat');
+        end
+    end
+end
