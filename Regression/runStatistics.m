e7c93dd000942caa9c9340ba865fc56e63a0b290
mbs0221 <mbs2021@163.com>
2018-04-11 14:29:46 +0800

    修改统计实验

--- Regression/runStatistics.m
+++ Regression/runStatistics.m
@@ -6,19 +6,19 @@ addpath(genpath('./utils'));
 addpath(genpath('./utils/params/'));
 
 % 加载数据集和网格搜索参数
+load('LabSVMReg.mat');
 load('LabUCIReg.mat');
 load('LabMulti.mat');
 load('LabIParams.mat');
 
 % 实验数据集
-LabDataSets = {LabUCIReg, LabMulti};
+LabDataSets = {LabSVMReg, LabUCIReg, LabMulti};
 % 统计每个数据集上的多任务实验数据
-index = 0;
-for i = 1 : 2
+m = length(LabDataSets);
+for i = 1 : m
     DataSets = LabDataSets{i};
     n = length(DataSets);
     for j = 1 : n
-        index = index + 1;
         DataSet = DataSets(j);
         [ LabStat, HasStat ] = LabStatistics(Path, DataSet, IParams);
         if HasStat == 1
