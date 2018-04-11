080d705178d0b5147f39c380c4a42b46b794d7e4
mbs0221 <mbs2021@163.com>
2018-04-11 15:50:30 +0800

    修改实验输出

--- Regression/runStatistics.m
+++ Regression/runStatistics.m
@@ -1,5 +1,4 @@
 Root = cd;
-Path = './data/';
 
 % 添加搜索路径
 addpath(genpath('./model'));
@@ -15,7 +14,6 @@ load('LabIParams.mat');
 % 实验数据集
 LabDataSets = {LabSVMReg, LabUCIReg, LabMulti};
 
-h = figure();
 % 统计每个数据集上的多任务实验数据
 m = length(LabDataSets);
 for i = 1 : m
@@ -23,7 +21,7 @@ for i = 1 : m
     n = length(DataSets);
     for j = 1 : n
         DataSet = DataSets(j);
-        [ LabStat, HasStat ] = LabStatistics(Path, DataSet, IParams);
+        [ LabStat, HasStat ] = LabStatistics(Root, DataSet, IParams);
         if HasStat == 1
             SaveStatitics( Root, DataSet, LabStat )
         end
