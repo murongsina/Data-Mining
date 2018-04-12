564cfcaa659f563d9bbe4310d99125515f92c6d8
mbs0221 <mbs2021@163.com>
2018-04-12 14:39:51 +0800

    添加大规模参数搜索

--- Regression/runCreateParams.m
+++ Regression/runCreateParams.m
@@ -1,36 +1,39 @@
 addpath(genpath('./utils/'));
 
-% 核函数参数
-RangeP1 = 2.^(-3:2:11)';
-kernel = struct('kernel', 'rbf', 'p1', RangeP1);
 
+% 核函数参数
+P1 = 2.^(-1:2:11)';
 % 分类器网格搜索参数
-C = 2.^(1:2:11)';
-C1 = 2.^(1:2:11)';
-C2 = 2.^(1:2:11)';
-C3 = 2.^(1:2:11)';
-C4 = 2.^(1:2:11)';
-EPS1 = 2.^(1:3:10)';
-EPS2 = 2.^(1:3:10)';
-RHO = 2.^(1:3:10)';
+C = 2.^(-1:2:11)';
+C1 = 2.^(-1:2:11)';
+C2 = 2.^(-1:2:11)';
+C3 = 2.^(-1:2:11)';
+C4 = 2.^(-1:2:11)';
+EPS1 = 2.^(-1:2:11)';
+EPS2 = 2.^(-1:2:11)';
+RHO = 2.^(-1:2:11)';
 LAMBDA = 2.^(-1:2:11)';
 GAMMA = 2.^(-1:2:11)';
-Nu = 2.^(-1:2:11)';
+NU = 2.^(-1:2:11)';
 
+% 核函数
+kernel = struct('kernel', 'rbf', 'p1', P1);
 % 任务参数
-Params1 = struct('Name', 'PSVR', 'nu', Nu, 'kernel', kernel);
+Params1 = struct('Name', 'PSVR', 'nu', NU, 'kernel', kernel);
 Params2 = struct('Name', 'TWSVR', 'C1', C1, 'C2', C2, 'C3', C3, 'C4', C4, 'eps1', EPS1, 'eps2', EPS2, 'kernel', kernel);
 Params3 = struct('Name', 'TWSVR_Xu', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'kernel', kernel);
-Params4 = struct('Name', 'MTL_PSVR', 'lambda', LAMBDA, 'nu', Nu, 'kernel', kernel);
-Params5 = struct('Name', 'MTL_LS_SVR', 'lambda', LAMBDA, 'gamma', GAMMA, 'kernel', kernel);
-Params6 = struct('Name', 'MTL_TWSVR', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'kernel', kernel);
-Params7 = struct('Name', 'MTL_TWSVR_Xu', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'kernel', kernel);
-Params8 = struct('Name', 'MTL_TWSVR_Mei', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'rho', RHO, 'lambda', LAMBDA, 'kernel', kernel);
+Params4 = struct('Name', 'LS_TWSVR', 'eps1', EPS1, 'eps2', EPS2, 'kernel', kernel);
+Params5 = struct('Name', 'MTL_PSVR', 'lambda', LAMBDA, 'nu', NU, 'kernel', kernel);
+Params6 = struct('Name', 'MTL_LS_SVR', 'lambda', LAMBDA, 'gamma', GAMMA, 'kernel', kernel);
+Params7 = struct('Name', 'MTL_TWSVR', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'kernel', kernel);
+Params8 = struct('Name', 'MTL_TWSVR_Xu', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'kernel', kernel);
+Params9 = struct('Name', 'MTL_TWSVR_Mei', 'C1', C1, 'C2', C2, 'eps1', EPS1, 'eps2', EPS2, 'rho', RHO, 'lambda', LAMBDA, 'kernel', kernel);
 
 % 转换参数表
 IParams = {
     Params1;Params2;Params3;Params4;
-    Params5;Params6;Params7;Params8
+    Params5;Params6;Params7;Params8;
+    Params9;
 };
 
 % 输出参数表信息
@@ -45,4 +48,4 @@ for i = 1 : nParams
 end
 
 % 保存参数表
-save('./params/LabIParams.mat', 'IParams');
+save('./params/LabIParams-Large.mat', 'IParams');
--- Regression/runCrossValid.m
+++ Regression/runCrossValid.m
@@ -11,15 +11,6 @@ addpath(genpath('./utils/'));
 load('LabMulti.mat');
 load('LabIParams.mat');
 
-% PSVR:28 params 0.04.
-% TWSVR:82944 params 38.24.
-% TWSVR_Xu:2304 params 0.35.
-% MTL_PSVR:196 params 0.10.
-% MTL_LS_SVR:196 params 0.14.
-% MTL_TWSVR:2304 params 0.22.
-% MTL_TWSVR_Xu:2304 params 0.13.
-% MTL_TWSVR_Mei:64512 params 3.84.
-
 % Êý¾Ý¼¯
 DataSetIndices = [3 4 10 11 12];
 ParamIndices = [7];
