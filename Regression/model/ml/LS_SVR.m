00dd5904bade285ec5ab185c03e528e42d27ac71
mbs0221 <mbs2021@163.com>
2018-04-13 20:31:41 +0800

    添加LS_SVR

---
+++ Regression/model/ml/LS_SVR.m
@@ -0,0 +1,28 @@
+function [ yTest, Time, w ] = LS_SVR(xTrain, yTrain, xTest, opts)
+%LS_SVR 此处显示有关此函数的摘要
+% Least Square Support Vector Regression
+%   此处显示详细说明
+
+%% Parse opts
+    gamma = opts.gamma;
+    kernel = opts.kernel;
+
+%% Fit
+    tic;
+    X = xTrain;
+    Y = yTrain;
+    Z = Kernel(X, X, kernel);
+    E = ones(size(Y));
+    I = diag(E);
+    H = Z*Z' + 1/gamma*I;
+    A = [H E;E' 0];
+    b = [Y; 0];
+    w = A\b;
+    Time = toc;
+    
+%% Predict
+    e = ones(length(xTest));
+    yTest = [Kernel(xTest, X, kernel) e]*w;
+    
+end
+
