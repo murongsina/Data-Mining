0a539e51e29595ce83bb43b198bcb9c811d303dc
mbs0221 <mbs2021@163.com>
2018-04-13 09:55:31 +0800

    调整任务排序和参数排序

--- Regression/runCreateMTL.m
+++ Regression/runCreateMTL.m
@@ -1,7 +1,7 @@
 addpath(genpath('./datasets'));
 addpath(genpath('./utils'));
 
-% load('LabReg.mat');
+load('LabReg.mat');
 
 % 数据集
 DataSetIndices = 1:5;
@@ -9,8 +9,17 @@ TaskNum = 2;
 Kfold = 5;
 
 % 构造多任务交叉验证
+Size = zeros(14, 1);
 for i = DataSetIndices
     LabReg(i) = MultiTask( LabReg(i), TaskNum, Kfold );
+    % 计算数据集任务大小
+    [ m, n ] = size(LabReg(i).Data);
+    TaskNum = LabReg(i).TaskNum;
+    Kfold = LabReg(i).Kfold;
+    Size(i,:) = m*n*TaskNum*Kfold;
 end
 
+% 按任务大小排序
+[ ~, IDX ] = sort(Size);
+LabReg = LabReg(IDX);
 save('./datasets/LabReg.mat', 'LabReg');
