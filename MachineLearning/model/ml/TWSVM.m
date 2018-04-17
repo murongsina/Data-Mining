81ec0ab951ede5ce795568a4e223f633be3c166f
mbs0221 <mbs2021@163.com>
2018-04-17 23:54:27 +0800

    组织文件

--- MachineLearning/model/ml/TWSVM.m
+++ MachineLearning/model/ml/TWSVM.m
@@ -1,11 +1,12 @@
-function [ yTest, Time ] = TWSVM(xTrain, yTrain, opts)
-%TWSVM 此处显示有关此类的摘要
-% Twin Support Vector Machine
+function [ yTest, Time ] = KTWSVM(xTrain, yTrain, xTest, opts)
+%KTWSVM 此处显示有关此类的摘要
+% Kernel Twin Support Vector Machine
 %   此处显示详细说明
     
 %% Parse opts
     C1 = opts.C1;
     C2 = opts.C2;
+    kernel = opts.kernel;
 
 %% Fit
     % 计时
@@ -15,39 +16,43 @@ function [ yTest, Time ] = TWSVM(xTrain, yTrain, opts)
     B = xTrain(yTrain==-1, :);
     [m1, ~] = size(A);
     [m2, ~] = size(B);
-    [~, n] = size(xTrain);
+    n = m1 + m2;
     e1 = ones(m1, 1);
     e2 = ones(m2, 1);
-    % 构造J,Q矩阵
-    J = [A e1];
-    Q = [B e2];
-    J2 = (J'*J);
-    Q2 = (Q'*Q);
-    % DTWSVM1
-    lb1 = zeros(m2,1);
-    ub1 = C1*ones(m2,1);
-    H1 = Q/J2*Q';
-    H1 = Utils.Cond(H1);
-    Alpha = quadprog(H1, -e2, [], [], [], [], lb1, ub1);
-    u = -J2\Q'*Alpha;
-    w1 = u(1:n);
-    b1 = u(end);
-    % DTWSVM2
+    % 最优化参数
+    options = optimset('Display', 'notify');
+    % 构造核矩阵
+    C = [A; B];
+    S = [Kernel(A, C, kernel) e1];
+    R = [Kernel(B, C, kernel) e2];
+    S2 = S'*S;
+    R2 = R'*R;
+    % KDTWSVM1
+%     S2 = Utils.Cond(S2);
+    H1 = R/S2*R';
+    lb1 = zeros(m2, 1);
+    ub1 = ones(m2, 1)*C1;
+    Alpha = quadprog(H1,-e2,[],[],[],[],lb1,ub1,[],options);
+    z1 = -S2\R'*Alpha;
+    u1 = z1(1:n);
+    b1 = z1(end);
+    % KDTWSVM2
+%     R2 = Utils.Cond(R2);
+    H2 = S/R2*S';
     lb2 = zeros(m1, 1);
-    ub2 = C2*ones(m1, 1);
-    H2 = J/Q2*J';
-    H2 = Utils.Cond(H2);
-    Beta = quadprog(H2, -e1, [], [], [], [], lb2, ub2);
-    v = -Q2\J'*Beta; 
-    w2 = v(1:n, :);
-    b2 = v(end);
+    ub2 = ones(m1, 1)*C2;
+    Mu = quadprog(H2,-e1,[],[],[],[],lb2,ub2,[],options);
+    z2 = -R2\S'*Mu;
+    u2 = z2(1:n);
+    b2 = z2(end);
     % 停止计时
     Time = toc;
-
+            
 %% Predict
-    D1 = abs(xTest*w1+b1);
-    D2 = abs(xTest*w2+b2);
+    K = Kernel(xTest, C, kernel);
+    D1 = abs(K*u1+b1);
+    D2 = abs(K*u2+b2);
     yTest = sign(D2-D1);
     yTest(yTest==0) = 1;
-    
+
 end
