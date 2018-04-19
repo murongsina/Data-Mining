3c761b3c7dade23cb5a36ccb84c0ec300eb5ed0f
mbs0221 <mbs2021@163.com>
2018-04-19 11:44:22 +0800

    添加算法

--- model/ml/TWSVM/LS_TWSVR_Huang.m
+++ model/ml/TWSVM/LS_TWSVR_Huang.m
@@ -1,6 +1,7 @@
-function [ yTest, Time, w ] = LS_TWSVR( xTrain, yTrain, xTest, opts )
+function [ yTest, Time, w ] = LS_TWSVR_Huang( xTrain, yTrain, xTest, opts )
 %LS_TWSVR 此处显示有关此函数的摘要
 % Primal least squares twin support vector regression
+% $Hua-juan HUANG^1, Shi-fei DING^{??1,2}, Zhong-zhi SHI^{2}$
 %   此处显示详细说明
 
 %% Parse opts
@@ -16,11 +17,11 @@ function [ yTest, Time, w ] = LS_TWSVR( xTrain, yTrain, xTest, opts )
     G = [Kernel(A, A, kernel) e];
     f = Y - eps1;
     h = Y + eps2;
-    GG = Cond(G'*G);
+    GGG = Cond(G'*G)\G';
     % LS_TWSVR1
-    u1 = GG\G'*f;
+    u1 = GGG*f;
     % LS_TWSVR2
-    u2 = GG\G'*h;
+    u2 = GGG*h;
     w = (u1+u2)/2;
     Time = toc;
     
---
+++ model/ml/TWSVM/LS_TWSVR_Mei.m
@@ -0,0 +1,53 @@
+function [ yTest, Time, w ] = LS_TWSVR_Mei(xTrain, yTrain, xTest, opts)
+%LS_TWSVR 此处显示有关此函数的摘要
+% Least Square Twin Support Vector Regression
+% Derived from TWSVR via TWSVM
+%   此处显示详细说明
+
+%% Parse opts
+    C1 = opts.C1;
+    C2 = opts.C1;
+    eps1 = opts.eps1;
+    eps2 = opts.eps1;
+    kernel = opts.kernel;
+    
+%% Prepare
+    tic;
+    A = xTrain;
+    Y = yTrain;
+    [m, ~] = size(A);
+    e = ones(m, 1);
+    C = A;
+    A = [Kernel(A, C, kernel) e];
+    % 得到f和g
+    f = Y + eps2;
+    g = Y - eps1;
+    % 得到Q矩阵
+    AAA = Cond(A'*A)\A';
+    H = A*AAA;
+
+%% Fit
+    I = eye(size(H));
+    % LS-TWSVR1
+    L1 = H - 1/C1*I;
+    R1 = g - H*f;
+    Alpha = L1\R1;
+    % LS-TWSVR2
+    L2 = 1/C2*I - H;
+    R2 = f - H*g;
+    Gamma = L2\R2;
+    % 得到u,v
+    u = AAA*(f+Alpha);
+    v = AAA*(g-Gamma);
+    % 得到w
+    w = (u+v)/2;
+    % 停止计时
+    Time = toc;
+    
+%% Predict
+    [m, ~] = size(xTest);
+    e = ones(m, 1);
+    yTest = [Kernel(xTest, C, kernel), e]*w;
+    
+end
+
