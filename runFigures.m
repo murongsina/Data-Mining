f3c8de15828bb9e4083bacf50e9d4af049087c05
mbs0221 <mbs2021@163.com>
2018-04-22 10:36:51 +0800

    修改图表

--- runFigures.m
+++ runFigures.m
@@ -7,11 +7,13 @@ addpath(genpath('./model'));
 addpath(genpath('./utils'));
 
 % 加载数据集和网格搜索参数
-load('LabReg.mat');
-load('LabIParams.mat');
+load('LabMTLClf.mat');
 
 % 实验数据集
-LabDataSets = {LabReg};
+LabDataSets = {LabMTLClf};
+
+% 实验设置
+opts = struct('Statistics', @ClfStat, 'IndexCount', 1);
 
 % 统计每个数据集上的多任务实验数据
 m = length(LabDataSets);
@@ -23,7 +25,7 @@ for i = 1 : m
         StatPath = [Path, './statistics/LabStat-', DataSet.Name, '.mat'];
         if exist(StatPath, 'file') == 2
             load(StatPath);
-            SaveFigures(Path, DataSet, LabStat, LabTime);
+            SaveFigures(Path, DataSet, LabStat, LabTime, opts);
         end
     end
 end
