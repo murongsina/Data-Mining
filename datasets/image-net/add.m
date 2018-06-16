ca896cd4f13c7167459d2e55e16e1a1d32f9920e
mbs0221 <mbs2021@163.com>
2018-06-16 12:17:05 +0800

    移除数据集预处理

--- datasets/image-net/add.m
+++
@@ -1,9 +0,0 @@
-function [ image_net ] = add( image_net, wnid, disc, type )
-%ADD 此处显示有关此函数的摘要
-%   此处显示详细说明
-
-    if isempty(find(strcmp(image_net(:,1), wnid), 1))
-        [m, ~] = size(image_net);
-        image_net(m + 1, :) = { wnid, disc, type };
-    end
-end
