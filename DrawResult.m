6f4c565965982ccc8c6ad1e4df6b8564c57e65db
mbs0221 <mbs2021@163.com>
2018-12-11 09:48:16 +0800

    统计图表

--- DrawResult.m
+++ DrawResult.m
@@ -1,19 +1,19 @@
-function [ ] = DrawResult(Stat, Time, labels, xTickLabel, arc)
+function [ ] = DrawResult(Stat, Time, label, labels, xTickLabel, arc)
 %DRAWRESULT 此处显示有关此函数的摘要
 % 绘制实验结果
 %   此处显示详细说明
     if nargin < 5
         arc = 0;
     end
-    subplot(2, 1, 1);
+    subplot(1, 2, 1);
     bar(Stat);
-    xlabel('#Task');
+    xlabel(label);
     ylabel('Accuracy (%)');
     legend(labels, 'Location', 'northwest');
     set(gca, 'XTicklabel', xTickLabel, 'XTickLabelRotation', arc);
-    subplot(2, 1, 2);
+    subplot(1, 2, 2);
     bar(Time);
-    xlabel('#Task');
+    xlabel(label);
     ylabel('Training time (ms)');
     legend(labels, 'Location', 'northwest');
     set(gca, 'XTicklabel', xTickLabel, 'XTickLabelRotation', arc);
--- paper2fig.m
+++ paper2fig.m
@@ -1,3 +1,4 @@
+opts = struct('mode', 'STL', '');
 h = figure();
 %%
 MyStat = MyStat*100;
@@ -16,47 +17,44 @@ CUR_IDX = MTL_IDX;
 IDX = 1;
 %% Monk
 xLabels = {'60', '90', '120', '150', '180', '210', '240', '270', 'All'};
-DrawResult(MyStat(CUR_IDX,[2:9,1],IDX)', MyTime(CUR_IDX,[2:9 1])', labels(CUR_IDX), xLabels);
+DrawResult(MyStat(CUR_IDX,[2:9,1],IDX)', MyTime(CUR_IDX,[2:9 1])', 'Task size', labels(CUR_IDX), xLabels);
 
 %% ISOLET
 xLabels = {'ab', 'cd', 'ef', 'gh', 'ij', 'kl','mn','op'};
-DrawResult(MyStat(CUR_IDX,10:17,IDX)', MyTime(CUR_IDX,10:17)', labels(CUR_IDX), xLabels);
+DrawResult(MyStat(CUR_IDX,10:17,IDX)', MyTime(CUR_IDX,10:17)', 'Category', labels(CUR_IDX), xLabels);
 
 %% Letter1
 xLabels = {'3', '5', '7', '9', '11'};
-DrawResult(MyStat(CUR_IDX,18:22,IDX)', MyTime(CUR_IDX,18:23)', labels(CUR_IDX), xLabels);
+DrawResult(MyStat(CUR_IDX,18:22,IDX)', MyTime(CUR_IDX,18:23)', 'Category', labels(CUR_IDX), xLabels);
 
 %% Letter2
 xLabels = {'T1', 'T2', 'T3', 'T4', 'T5'};
-DrawResult(MyStat(CUR_IDX,23:27,IDX)', MyTime(CUR_IDX,23:27)', labels(CUR_IDX), xLabels);
+DrawResult(MyStat(CUR_IDX,23:27,IDX)', MyTime(CUR_IDX,23:27)', 'Category', labels(CUR_IDX), xLabels);
 
 %% Caltech
 xLabels = {'Birds_1','Insects_1','Flowers_1','Mammals_1','Instruments_1','Aircrafts','Balls','Bikes','Birds','Boats','Flowers','Instruments','Plants','Mammals','Vehicles'};
-DrawResult(MyStat(CUR_IDX,:,IDX)', MyTime(CUR_IDX,:)', labels(CUR_IDX), xLabels, 45);
+DrawResult(MyStat(CUR_IDX,:,IDX)', MyTime(CUR_IDX,:)', 'Category', labels(CUR_IDX), xLabels, 45);
 
 %% Caltech101
 % (5x363),(4x368),(3x348),(10x430),(6x411)
 xLabels = {'Birds','Insects','Flowers','Mammals','Instruments'};
-DrawResult(MyStat(CUR_IDX,1:5,IDX)', MyTime(CUR_IDX,1:5)', labels(CUR_IDX), xLabels, 0);
+DrawResult(MyStat(CUR_IDX,1:5,IDX)', MyTime(CUR_IDX,1:5)', 'Category', labels(CUR_IDX), xLabels, 45);
 
 %% Caltech256
-xLabels = {'Aircrafts\newline(4x466)','Balls\newline(5x445)','Bikes\newline(6x381)',...
-    'Birds\newline(9x431)','Boats\newline(4x383)','Flowers\newline(3x381)',...
-    'Instruments\newline(5x409)','Plants\newline(4x392)','Mammals\newline(10x502)',...
-    'Vehicles\newline(9x495)'};
-DrawResult(MyStat(CUR_IDX,6:15,IDX)', MyTime(CUR_IDX,6:15)', labels(CUR_IDX), xLabels, 45);
+xLabels = {'Aircrafts','Balls','Bikes', 'Birds','Boats','Flowers', 'Instruments','Plants','Mammals', 'Vehicles'};
+DrawResult(MyStat(CUR_IDX,6:14,IDX)', MyTime(CUR_IDX,6:14)', 'Category', labels(CUR_IDX), xLabels, 45);
 
 %% Flags
 xLabels = {'100', '120', '140', '160', '180', '191'};
-DrawResult(MyStat(CUR_IDX,1:6,IDX)', MyTime(CUR_IDX,1:6)', labels(CUR_IDX), xLabels);
+DrawResult(MyStat(CUR_IDX,1:6,IDX)', MyTime(CUR_IDX,1:6)', 'Task size', labels(CUR_IDX), xLabels);
 
 %% Emotions
 xLabels = {'100', '120', '140', '160', '180', '200'};
-DrawResult(MyStat(CUR_IDX,7:12,IDX)', MyTime(CUR_IDX,7:12)', labels(CUR_IDX), xLabels);
+DrawResult(MyStat(CUR_IDX,7:12,IDX)', MyTime(CUR_IDX,7:12)', 'Task size', labels(CUR_IDX), xLabels);
 
 %% Flags-Each
 xLabels = {'1', '2', '3', '4', '5', '6', '7'};
-DrawResult(MyStat(CUR_IDX,1:7,IDX)', MyTime(CUR_IDX,1), labels(CUR_IDX), xLabels);
+DrawResult(MyStat(CUR_IDX,1:7,IDX)', MyTime(CUR_IDX,1), 'Task index', labels(CUR_IDX), xLabels);
 
 %% Emotions-Each
 xLabels = {'1', '2', '3', '4', '5', '6'};
