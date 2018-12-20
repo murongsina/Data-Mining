12ddad6ccc73c57d280d57dd3770710ddd384d5c
mbs0221 <mbs2021@163.com>
2018-12-20 11:24:25 +0800

    实验

--- LoadTask.m
+++ LoadTask.m
@@ -7,13 +7,15 @@ load('MTL_UCI5.mat');
 IParams = CreateParams(CParams{10});
 %%
 Error = cell(10, 1);
-for i = [4]
+for i = [2:9]
     D = MTL_UCI5(i);
     A = load(['RMTL-', D.Name,'.mat']);
     B = load(['SSR_RMTL-', D.Name,'.mat']);
     C = mean(A.CVStat-B.CVStat, 3);
-    if sum(C(:,1)) > 0
+    E = sum(C(:,1));
+    if E > 0
         Error{i} = [mean([A.CVStat(:,1,:), B.CVStat(:,1,:)], 3), C(:,1), B.CVRate];
+        fprintf('Total %.4f Error in %d:\n', E, i);
     end
 end
 %%
